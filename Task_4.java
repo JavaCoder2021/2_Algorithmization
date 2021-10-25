506b618e6e8c7fc71592d64be40cf9ebbabafaf9
JavaCoder2021 <92649923+JavaCoder2021@users.noreply.github.com>
2021-10-25 17:58:58 +0200

    Update Task_4.java

--- Task_4.java
+++ Task_4.java
@@ -10,7 +10,7 @@ public class Task_4 {
     public static void main(String[] args) {
         
         Scanner input = new Scanner(System.in);
-        System.out.print("Количество элементов массива = ");
+        System.out.print("Amount of elements = ");
         int n = input.nextInt();
         
         double[] myArray = new double[n];
@@ -19,15 +19,16 @@ public class Task_4 {
         double maxA = 0;
         double minA = 0;
              
-        //Создаём массив
+        //Create an array
+        System.out.println("Array:");
         for (int i = 0; i < n; i++)
         {
             myArray[i] = (double) (Math.random() * 20) - 10;
-            if (i < n-1) System.out.printf(" %.1f  ", myArray[i]);
-            else System.out.printf(" %.1f\n", myArray[i]);
+            System.out.printf(" %.1f  ", myArray[i]);
         }
+        System.out.println("");
         
-        //Вычисления 
+        //Calculations 
         for (int i = 0; i < n; i++)
         {
             if (i==0) {
@@ -38,13 +39,13 @@ public class Task_4 {
             }
             else
             {
-                //Ищем максимальный элемент
+                //Max element
                 if (myArray[i] > maxA) 
                 {
                     maxN = i;
                     maxA = myArray[i]; 
                 }
-                //Ищем минимальный элемент
+                //Min element
                 if (myArray[i] < minA) 
                 {
                     minN = i;
@@ -53,16 +54,16 @@ public class Task_4 {
             }
         }  
         
-        //Меняем элементы местами
+        //Swapping elements in places
         myArray[maxN] = minA;
         myArray[minN] = maxA;
         
-        //Выводим массив
+        System.out.println("Final array:");
         for (int i = 0; i < n; i++)
         {
-            if (i < n-1) System.out.printf(" %.1f  ", myArray[i]);
-            else System.out.printf(" %.1f\n", myArray[i]);
+            System.out.printf(" %.1f  ", myArray[i]);
         }
+        System.out.println("");
         
     }
     
