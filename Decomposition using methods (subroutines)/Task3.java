fd056b3b6d47630c6e426ecb4a8a6906c1ec470d
JavaCoder2021 <92649923+JavaCoder2021@users.noreply.github.com>
2021-10-30 13:14:19 +0200

    Create Task3.java

---
+++ Decomposition using methods (subroutines)/Task3.java
@@ -0,0 +1,39 @@
+//3.  Вычислить  площадь  правильного  шестиугольника  со  стороной  а,  
+//используя  метод  вычисления  площади треугольника. 
+
+package task3;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class Task3 {
+      
+    public static void main(String[] args) {
+        
+        //Input
+        Scanner input = new Scanner(System.in);
+        
+        double a;
+        System.out.print("a = ");
+        a = input.nextDouble();
+        while (a <= 0)
+        {
+            System.out.print("a = ");
+            a = input.nextDouble();
+        }
+        
+        Random random = new Random();
+        
+        //Calculations
+        double area = hexagonArea(a);
+            
+        //Output
+        System.out.printf("area = %.2f \n", area);
+
+    }
+    
+    public static double hexagonArea(double a) {
+        return (6 * (a * a))/(4 * Math.tan(Math.PI / 6));
+    }
+    
+}
