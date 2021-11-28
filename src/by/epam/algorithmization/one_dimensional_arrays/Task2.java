63d10a9f022180745033ebc29ad087d0b3d6b9e8
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2021-11-28 15:37:35 +0200

    Create Task2.java

---
+++ src/by/epam/algorithmization/one_dimensional_arrays/Task2.java
@@ -0,0 +1,61 @@
+/*
+ * 2. 
+ * Дана последовательность действительных чисел а1 ,а2 ,..., аn. 
+ * Заменить все ее члены, большие данного Z, этим числом. Подсчитать количество замен
+ */
+package by.epam.basics.algorithmization.one_dimensional_arrays;
+
+import java.util.Scanner;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+        
+        int n    = scannerInt("Amount of elements = ");
+        double Z = scannerInt("Z = ");
+   
+        double[] myArray = new double[n];
+        int summ = 0;
+        
+        //Create an array
+        System.out.println("Array:");
+        for (int i = 0; i < n; i++) {
+            myArray[i] = (double) (Math.random() * 9 + 1);
+            System.out.printf(" %.1f  ", myArray[i]);
+            //Calculations
+            if (myArray[i] > Z) {
+                myArray[i] = Z;
+                summ++;
+            }
+        }
+        System.out.println();
+                   
+        System.out.println("Amount of elements > Z = " + summ);
+        for (int i = 0; i < n; i++)
+        {
+            System.out.printf(" %.1f  ", myArray[i]);            
+        }
+        System.out.println("");
+      
+    }
+    
+    private static int scannerInt(String str) {
+        
+        Scanner input = new Scanner(System.in);
+        int num = -1;
+        
+        do {
+            System.out.print(str);
+            if (input.hasNextInt()) {
+                num = input.nextInt();
+            } 
+            else {
+                input.next();
+            }
+        } while (num <= 0);
+
+        return num;
+        
+    }
+    
+}
