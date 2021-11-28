744668b405ff1ce12a60c89fbce9295212f96fcb
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2021-11-28 17:37:35 +0200

    Update Task1.java

--- src/by/epam/algorithmization/arrays_of_arrays/Task1.java
+++ src/by/epam/algorithmization/arrays_of_arrays/Task1.java
@@ -1,1 +1,68 @@
-//
+/*
+ * 1. 
+ * Дана матрица. 
+ * Вывести на экран все нечетные столбцы, у которых первый элемент больше последнего.
+ */
+package by.epam.algorithmization.arrays_of_arrays;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        
+        //Input
+        int n = scannerInt("Elements in an array: ");
+        
+        Random random = new Random();
+        int[][] myArray = new int[n][n];
+ 
+        //Create an array
+        System.out.println("Array:");
+        for (int i = 0; i < myArray.length; i++) {
+            for (int a = 0; a < myArray.length; a++) {
+                myArray[i][a] = random.nextInt(20);
+                System.out.print(myArray[i][a] + "\t");
+            }
+            System.out.println();
+        }
+           
+        //Output
+        System.out.println("Output:");
+        for (int i = 0; i < n; i++) {
+            for (int a = 0; a < n; a++) {
+                if (a % 2 == 0) {
+                    if (myArray[0][a] > myArray[n-1][a])
+                        System.out.print(myArray[i][a] + "\t");
+                    else 
+                        System.out.print("\t");
+                }
+                else 
+                    System.out.print("\t");
+            }
+            System.out.println();
+        }
+            
+    }
+    
+    private static int scannerInt(String str) {
+        
+        Scanner input = new Scanner(System.in);
+        int num = -1;
+        
+        do {
+            System.out.print(str);
+            if (input.hasNextInt()) {
+                num = input.nextInt();
+            } 
+            else {
+                input.next();
+            }
+        } while (num <= 0);
+
+        return num;
+        
+    }    
+    
+}
