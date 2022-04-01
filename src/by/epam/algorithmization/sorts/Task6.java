659fb5098434abdfd84001a5c78ca4ec6f41c28a
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2022-04-01 19:00:35 +0200

    Update Task6.java

--- src/by/epam/algorithmization/sorts/Task6.java
+++ src/by/epam/algorithmization/sorts/Task6.java
@@ -5,74 +5,42 @@
  * Требуется упорядочить его по возрастанию. 
  * Делается это следующим образом: сравниваются два соседних элемента 
  */
-package by.epam.algorithmization.sorts;
-
-import java.util.Scanner;
+package by.epam.algorithmization.sort;
 
 public class Task6 {
-    
-    public static void main(String[] args) {
-        
-        //Input        
-        int n = scannerInt("Array size = ");
 
-        double[] myArray = new double[n];
-        int i = 0;
-        
-        //Create array
-        System.out.println("Array:");
-        for (i = 0; i < n; i++) {
-            myArray[i] = Math.random() * 10;
-            System.out.printf(" %.1f ", myArray[i]);
-        }    
-        System.out.println();
-                
-        //Calculations
-        double tmp = 0;
-        i = 0;
-        int count = 0;
-        while (i < myArray.length - 1) {
-            if (myArray[i] <= myArray[i + 1]) {
-		i++;
-	    } 
-            else {
-                count = i;
-                while(myArray[count] > myArray[count + 1]) {    
-                    tmp = myArray[count];
-                    myArray[count] = myArray[count + 1];
-                    myArray[count + 1] = tmp;
-                    count--;
-                    if (count < 0) 
-                        break;
-                }
-            }
-        }
-        
-        //Output
-        System.out.println("Output array:");
-        for (i = 0; i < myArray.length; i++)
-            System.out.printf(" %.1f ", myArray[i]);
-        System.out.println();
-        
-    }
-    
-    private static int scannerInt(String str) {
-        
-        Scanner input = new Scanner(System.in);
-        int num = -1;
-        
-        do {
-            System.out.print(str);
-            if (input.hasNextInt()) {
-                num = input.nextInt();
-            } 
-            else {
-                input.next();
-            }
-        } while (num <= 0);
+	public static void main(String[] args) {
 
-        return num;
-        
-    }
-    
+		double[] myArray = new double[10];
+		int i, count = 0;
+		double tmp = 0;
+
+		for (i = 0; i < myArray.length; i++) {
+			myArray[i] = Math.random() * 10;
+			System.out.printf(" %.1f ", myArray[i]);
+		}
+		System.out.println();
+
+		i = 0;
+		while (i < myArray.length - 1) {
+			if (myArray[i] <= myArray[i + 1]) {
+				i++;
+			} else {
+				count = i;
+				while (myArray[count] > myArray[count + 1]) {
+					tmp = myArray[count];
+					myArray[count] = myArray[count + 1];
+					myArray[count + 1] = tmp;
+					count--;
+					if (count < 0)
+						break;
+				}
+			}
+		}
+
+		for (i = 0; i < myArray.length; i++)
+			System.out.printf(" %.1f ", myArray[i]);
+
+	}
+
 }
